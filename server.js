63fbb3251a57dbe3444b903032bc21a517f26284
rob-cosentino <robert.cosentino1@gmail.com>
2023-12-01 15:06:11 -0500

    NothingBurger

--- server.js
+++ server.js
@@ -38,6 +38,6 @@ app.get('/api/:guitarist', (req, res) => {
     }
 })
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || PORT, () => {
     console.log(`The server is now running on port ${PORT}`)
 })
